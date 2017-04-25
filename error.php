0355f296b6571076931ecf12cbf3143088cb2a1d
BodruzicMatej <bodruzic.matej@gmail.com>
2017-04-25 22:18:49 +0200

    Add files via upload

---
+++ error.php
@@ -0,0 +1,14 @@
+<?php
+	require_once 'core/init.php';
+	
+	Helper::getHeader('ERROR | Algebra Contacts');
+?>
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+	<iframe frameBorder="0" src="/AlgebraContactsApp/errors/404.html"></iframe>
+	</div>
+</div>
+	
+<?php
+	Helper::getFooter();
+?>
